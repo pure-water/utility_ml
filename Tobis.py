f5eb8124a047e4e516f061639d8a8bd7f83f7093
Ubuntu <ubuntu@ip-172-30-0-49.ec2.internal>
2018-05-06 06:52:42 +0000

    adding true box

--- Tobis.py
+++ Tobis.py
@@ -9,11 +9,18 @@ from keras.optimizers import SGD, Adam, RMSprop
 import numpy as np
 from keras.utils import plot_model
 
+#import keras
+#def get_trainable_params(model):
+#    params = []
+#    for layer in model.layers:
+#        params += keras.engine.training.collect_trainable_weights(layer)
+#    return params
 
 
 run_meta = tf.RunMetadata()
 
 input_size= 416
+max_box_per_image = 10
 
 
 with tf.Session(graph=tf.Graph()) as sess:
@@ -21,48 +28,31 @@ with tf.Session(graph=tf.Graph()) as sess:
     K.set_session(sess)
 
     input_image = Input(shape=(input_size, input_size, 3),name="input1")
+    true_boxes  = Input(shape=(1, 1, 1, max_box_per_image , 4)) 
+
+
+
 
     mobilenet  = MobileNet(input_tensor=tf.placeholder('float32',shape=(1,input_size,input_size,3)),include_top=False,input_shape=(input_size,input_size,3),weights=None)(input_image)
-    #mobilenet = MobileNet(include_top=False,input_shape=(input_size,input_size,3),weights=None)
-    detect = Conv2D(30,(1,1),strides=(1,1),padding='same',name='DetectoinLayer0')(mobilenet)
+    detect  = Conv2D(30,(1,1),strides=(1,1),padding='same',name='DetectoinLayer0')(mobilenet)
     detect  = Reshape((13, 13, 5, 6))(detect)
-
+    detect  = Lambda(lambda args: args[0])([detect, true_boxes])
     
-    yolo = Model(input_image,detect)
+    yolo = Model([input_image,true_boxes],detect)
     plot_model(yolo, to_file='yolo.png')
     print(detect)
     yolo.summary()
 
     optimizer = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     yolo.compile(loss='mean_squared_error', optimizer=optimizer)
-    
-
-    #sess.run(feature)
-    #mobilenet = MobileNet(input_tensor=tf.placeholder('float32', shape=(1,224,224,3)),include_top=False,input_shape=(input_size,input_size,3))
-    #mobilenet = MobileNet(include_top=False,input_shape=(input_size,input_size,3))
-    #net.get_output_shape()
-    #m_feature = mobilenet(input_image)
-
-    #print(mobilenet.outputs)
-    #print(mobilenet.layers)
 
 
+    #yolo_params = get_trainable_params(yolo)
+    #param_grad = tf.gradients(detect, yolo_params)
 
-    #learning_rate = 0.1
-    #optimizer = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-
-    #output = Conv2D(30, 
-    #                     (1,1), strides=(1,1), 
-    #                     padding='same', 
-    #                     name='DetectionLayer'
-    #                 )(m_feature)
- 
-
-    #mobilenet.compile(loss='mean_squared_error', optimizer=optimizer)
-
-
+    #set up the train data
     #X=np.random.rand(input_size,input_size,3)
-    #mobilenet.fit(x=X,steps_per_epoch=1)
+    #yolo.fit(x=X,steps_per_epoch=1)
     #model.fit(steps_per_epoch=1)
     #mobilenet.evaluate(x=X)
 
